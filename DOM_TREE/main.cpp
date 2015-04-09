cc95f7e88ae53e3b1924272f453cfdc84b8f6438
vsanchez93 <victorsanchez1993@gmail.com>
2015-04-08 21:06:35 -0430

    main

--- DOM_TREE/main.cpp
+++ DOM_TREE/main.cpp
@@ -9,22 +9,22 @@ int main()
 
 	DOM_tree<element> d,aux,aux1;
 	element e;
-	string cadena="<p>Este es un <b>string <x>hola</x><y>cagon</y></b><t> joder rat kid</t></p>",cadena1="<x>Este<y>string</y></x>";
+	string cadena="<html><head><title>Ejemplo de Página Web</title></head><body><h1>Una Página Web</h1></body></html>";
 	d.appendChild(cadena);
-	e.settagna("jeus");
-	e.setinnerHTML("daniel");
-	aux.appendChild(cadena1);
+	//e.settagna("jeus");
+	//e.setinnerHTML("daniel");
+	//aux.appendChild(cadena1);
 
-	d.appendChild(e);
-	e.settagna("jeus1");
-	e.setinnerHTML("daniel1");
-	d.appendChild(e);
+	//d.appendChild(e);
+	//e.settagna("jeus1");
+	//e.setinnerHTML("daniel1");
+	//d.appendChild(e);
 	//d.appendChild(cadena);
 	//d.replaceChild(1,aux);
 
 	//aux1=d.getElementByID("x");
 	d.mostrar();
-    cout << "Hello world!" << endl;
+    //cout << "Hello world!" << endl;
 
     return 0;
 }
