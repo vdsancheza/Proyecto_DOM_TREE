7c0a2f4f61a23a91bdb9e8967c41c5c832f83566
vsanchez93 <victorsanchez1993@gmail.com>
2015-04-09 19:59:13 -0430

    3:16 pm

--- DOM_TREE/main.cpp
+++ DOM_TREE/main.cpp
@@ -11,23 +11,7 @@ int main()
 	element e;
 	string cadena="<html><head><title>Ejemplo de Página Web</title></head><body><h1>Una Página Web</h1><p id='contenido'>Hola Mundo!!!</p><p>Bye!!!</p></body></html>";
 	d.appendChild(cadena);
-	///
-	//std::cout << "Dom_Tree: " << d << std::endl;7
-	///
-	//e.settagna("jeus");
-	//e.setinnerHTML("daniel");
-	//aux.appendChild(cadena1);
-
-	//d.appendChild(e);
-	//e.settagna("jeus1");
-	//e.setinnerHTML("daniel1");
-	//d.appendChild(e);
-	//d.appendChild(cadena);
-	//d.replaceChild(1,aux);7
-
-	//aux1=d.getElementByID("x");
 	d.mostrar();
-    //cout << "Hello world!" << endl;
 
     return 0;
 }
